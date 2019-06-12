bb049982ae82a7519e4dc67b0c9697e923216dd7
Plant Lady <codeplantsbylouie@gmail.com>
2019-06-12 11:53:57 -0400

    added ifs and the methods and consoles work

--- currentDate/main.js
+++ currentDate/main.js
@@ -1,10 +1,28 @@
 let today = new Date();
 console.log(today);
 
-console.log(today.getMonth());
+// console.log(today.getMonth());
 // outputs 5 --> this is because January starts with 0
-console.log(today.getDate());
+// console.log(today.getDate());
 // outputs 12 --> today is the 12th
-console.log(today.getFullYear());
+// console.log(today.getFullYear());
 // outputs 2019 --> that is the year
 
+let month = today.getMonth() + 1;
+if (month < 10) {
+  month = '0' + month;
+}
+// console.log(month);
+
+let day = today.getDate();
+if (day < 10) {
+  day = '0'+day;
+}
+// console.log(day);
+
+let year = today.getFullYear();
+// console.log(year);
+
+// Get Date
+let todaysDate = month + "/" + day + "/" + year;
+console.log(todaysDate);
