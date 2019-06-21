3f41ff5612fabcef22230266a67dcad76cb1392f
Plant Lady <codeplantsbylouie@gmail.com>
2019-06-21 18:18:56 -0400

    attempt to do rotating string exercise; proving to be more diffiuclt than i thought; more thought and playing around in the mean time

--- rotateString/main.js
+++ rotateString/main.js
@@ -1,5 +1,22 @@
-let string = "ThisIsATest";
+let str = "abcd";
+console.log(str);
+let str1 = str.slice(3);
+console.log(str1);
 
-function animate_string(st) {
 
-}
+
+
+
+
+// // EXTRA PRACTICE
+// // changing str = abcd to uppercase
+// let upperStr = str.toUpperCase();
+// console.log(upperStr);
+
+// // spliting str = abcd; strSplit = "This is a test for the split method"
+// let splitStr = str.split();
+// console.log(splitStr);
+
+// let strString = "This is a test for the split method";
+// let strSplit = strString.split(" ");
+// console.log(strSplit);
