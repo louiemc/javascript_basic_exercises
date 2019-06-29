2a65427355c58be473d0fa029862a1dc6d94efdf
Plant Lady <codeplantsbylouie@gmail.com>
2019-06-28 20:21:05 -0400

    Solved! all by myself with mdn help; comments in code; next up is adding explanation and screenshots to readme

--- firstJanuary/main.js
+++ firstJanuary/main.js
@@ -1,99 +1,10 @@
-// let date = new Date();
-// console.log(date);
-
-// let weekDay = date.getDay();
-// console.log(weekDay);
-
-// switch (weekDay) {
-//   case 0:
-//     console.log("Today is Sunday");
-//     break;
-//   case 1:
-//     console.log("Today is Monday");
-//     break;
-//   case 2:
-//     console.log("Today is Tuesday");
-//     break;
-//   case 3:
-//     console.log("Today is Wednesday");
-//     break;
-//   case 4:
-//     console.log("Today is Thursday");
-//     break;
-//   case 5:
-//     console.log("Today is Friday");
-//     break;
-//   case 6:
-//     console.log("Today is Saturday");
-//     break;
-// }
-
-// let yearFull = date.getFullYear();
-// console.log("Year:", yearFull);
-
-// let month = date.getMonth() + 1;
-// console.log(month);
-// switch (month) {
-//   case 1:
-//     console.log("Month: January ");
-//     break;
-//   case 2:
-//     console.log("Month: February ");
-//     break;
-//   case 3:
-//     console.log("Month: March ");
-//     break;
-//   case 4:
-//     console.log("Month: April ");
-//     break;
-//   case 5:
-//     console.log("Month: May ");
-//     break;
-//   case 6:
-//     console.log("Month: June ");
-//     break;
-//   case 7:
-//     console.log("Month: July ");
-//     break;
-//   case 8:
-//     console.log("Month: August ");
-//     break;
-//   case 9:
-//     console.log("Month: September ");
-//     break;
-//   case 10:
-//     console.log("Month: October ");
-//     break;
-//   case 11:
-//     console.log("Month: November ");
-//     break;
-//   case 12:
-//     console.log("Month: December ");
-//     break;
-// };
-
-
-// let dayNumber = date.getDate();
-// console.log(dayNumber);
-
-
-// // Date: month, weekDay, year
-// console.log(month, dayNumber, yearFull);
-
-// console.log("///////////////////////////////////////////////////");
-// // my semi-solution
-// function firstSunday(year) {
-//   if (weekDay === 0 && dayNumber === 1) {
-//     console.log("This January 1st lands on a Sunday!", year);
-//   } else {
-//     console.log("January 1st does NOT land on a Sunday!");
-//   }
-// }
-// console.log(firstSunday(2017));
-// console.log(firstSunday(2045));
-// console.log(firstSunday(2050));
-// console.log(firstSunday(2025));
-// console.log("///////////////////////////////////////////////////");
+// NOTE: SYNTAX FOR DATE METHOD
+// new Date(year, monthIndex [, day [, hours [, minutes [, seconds [, milliseconds]]]]]);
+// year: integer value representing the year; 
+// monthIndex: integer value representing the month, 0 for january to `11 for December
+// day: integer value representing the day of the month. if not specified, the default value of 1 is used
+// hours: intger value representing the hour of the day; default is 0 (midnight)
+//  minutes, seconds, millisenconds: integer value; default is 0
 
 console.log("***************************************************");
 
@@ -101,20 +12,22 @@ let x = 2014;
 let y = 2050;
 
 for (var yearStart = x; yearStart <= y; yearStart++) {
-  console.log(yearStart);
+  // declare variable z to equal new Date method
+  // yearStart represents the year parameter
+  // 0 represents January (monthIndex)
+  // 1 represents January 1st; bc day represents the day of the month
+  // SO WE WANT: we want to know if January 1st falls on a SUNDAY
+  let z = new Date(yearStart, 0, 1);
+  // to get sunday: use getDay(method); so *if* you getDay of z and if it's === 0 (sunday) then console.log(year that jan 1st is a sunday)
+  if (z.getDay() === 0) {
+    console.log("January 1st on a Sunday:", yearStart);
+  }
 }
 
-// need to 
-// for (var year = 2014; year <= 2050; i++) {
 
-// }
 
 console.log("***************************************************");
 
-
-
-
-
-// years between 2014 to 2050
-// let thisYear = firstSunday(2017);
-// console.log(thisYear);
+// // getDay() method
+// let getDay1 = new Date();
+// console.log(getDay1.getDay()); // output 5, which is Friday; so Sunday is 0 (goes from 0 - 6)
