0d87317681eb101e426992e9dbce1e9d8736ac45
Plant Lady <codeplantsbylouie@gmail.com>
2019-07-01 15:19:30 -0400

    array of numbers, math random and math floor working

---
+++ randomNumber/main.js
@@ -0,0 +1,11 @@
+// Create an array with numbers 1 to 10
+let x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// declare a variable to hold the random number
+// using Javascript built in math functions: Math.floor and Math.random
+let randomNumber = Math.floor(Math.random() * x.length) + 1;
+console.log(randomNumber); // outputs random number
+
+let userGuess;
+
+
