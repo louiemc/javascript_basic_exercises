52f5c9a964e30739e95f1289b9f7025d19063a53
Plant Lady <codeplantsbylouie@gmail.com>
2019-07-02 13:36:12 -0400

    DONE! seconds counted and converted into days to get daysBetween; getting timeBetween: gets the time difference in milliseconds, don't necessarily need abs but days are always positive

--- daysTilChristmas/main.js
+++ daysTilChristmas/main.js
@@ -1,13 +1,26 @@
+// y for date2
+const y = '12/25/2019';
+
 // get the date of today
-let date1 = new Date();
-console.log(presentDate); // outputs YYY-MM-DD and the time?
+const date1 = new Date();
+console.log(date1); // outputs YYY-MM-DD and the time?
+
 // assign value (date you want to compare)
-let date2 = new Date('December 25, 2019');
+const date2 = new Date(y);
 console.log(date2);
 
+// time between the 2 dates
+// getting the amount of time that is passed brom date2 and date1
+const timeBetween = Math.abs(date2.getTime() - date1.getTime());
+console.log(timeBetween);
+
+// find days between using the timeBetween
+// BUT timeBetween NEEDS to be divided by 1000 * 3600 * 24 which is the number of milliseconds per day.
+const daysBetween = Math.ceil(timeBetween / (1000 * 60 * 60 * 24));
+console.log("Days from now until Christmas:", daysBetween);
 // *******************************************
 // declaring another variable to hold the end date variable
-let  = x
-console.log(endDate);
+// let  = x
+// console.log(endDate);
 
 
