0ef183badb882689a237cecb9802184271c5fb8f
Plant Lady <codeplantsbylouie@gmail.com>
2019-07-02 12:26:29 -0400

    presentDate and endDate console.log-ing; may need to change variables

---
+++ daysTilChristmas/main.js
@@ -0,0 +1,12 @@
+// get the date of today
+let presentDate = new Date();
+console.log(presentDate); // outputs YYY-MM-DD and the time?
+// assign value (date you want to compare)
+let x = new Date('December 25, 2019');
+
+// *******************************************
+// declaring another variable to hold the end date variable
+let endDate = x
+console.log(endDate);
+
+
