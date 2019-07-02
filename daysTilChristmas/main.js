55e8d15f0aaf892e48c34d2dff1f7f01cc127cf2
Plant Lady <codeplantsbylouie@gmail.com>
2019-07-02 12:40:10 -0400

    changed date variables to date1 and date2

--- daysTilChristmas/main.js
+++ daysTilChristmas/main.js
@@ -1,12 +1,13 @@
 // get the date of today
-let presentDate = new Date();
+let date1 = new Date();
 console.log(presentDate); // outputs YYY-MM-DD and the time?
 // assign value (date you want to compare)
-let x = new Date('December 25, 2019');
+let date2 = new Date('December 25, 2019');
+console.log(date2);
 
 // *******************************************
 // declaring another variable to hold the end date variable
-let endDate = x
+let  = x
 console.log(endDate);
 
 
