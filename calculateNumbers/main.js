bf1bdde95393c88c21a4a34b4b9d16a79fba5969
Plant Lady <codeplantsbylouie@gmail.com>
2019-07-04 18:50:07 -0400

    addingthe parameters to the empty parentheses

--- calculateNumbers/main.js
+++ calculateNumbers/main.js
@@ -27,19 +27,19 @@ function add(value1, value2) {
 add(1, 5);
 
 
-function subtract() {
+function subtract(value1, value2) {
   let difference = value1 + value2;
   console.log(difference);
 }
 subtract(10, 7);
 
-function multiply() {
+function multiply(value1, value2) {
   let product = value1 + value2;
   console.log(product);
 }
 multiply(5, 6);
 
-function divide() {
+function divide(value1, value2) {
   let quotient = value1 + value2;
   console.log(quotient);
 }
