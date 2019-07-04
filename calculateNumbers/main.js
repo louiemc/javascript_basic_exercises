fd7bc097d0f1382c6ef7684a070a466075aaace1
Plant Lady <codeplantsbylouie@gmail.com>
2019-07-03 21:32:51 -0400

    thinking of using a swtich statement so the program can recognize addition, subtraction, multiplication, division, and then carry out functions specific to the cases

---
+++ calculateNumbers/main.js
@@ -0,0 +1,17 @@
+
+
+// add a switch statement, so if +, -, *, / then carry out those functions
+switch(calculate) {
+  case +:
+    function add();
+    break;
+  case -:
+    function subtract();
+    break;
+  case *:
+    function multiply();
+    break;
+  case /:
+    function divide();
+    break;
+}
