f10cd044e64591a9024cec5b692cce61fdd46bc0
Plant Lady <codeplantsbylouie@gmail.com>
2019-07-04 19:14:15 -0400

    deleted the switch statement; maybe a inquirer.prompt to ask the user for value1 and then value2 and then the arithmetic option

--- calculateNumbers/main.js
+++ calculateNumbers/main.js
@@ -1,23 +1,5 @@
 // const inquirer = require(inquirer);
 
-// add a switch statement, so if +, -, *, / then carry out those functions
-// switch(calculate) {
-//   case "+":
-//     function add();
-//     break;
-//   case "-":
-//     function subtract();
-//     break;
-//   case "*":
-//     function multiply();
-//     break;
-//   case "/":
-//     function divide();
-//     break;
-//   default:
-//     console.log('test test test test');
-//     break;
-// }
 
 
 function add(value1, value2) {
