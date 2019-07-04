01ddbe0cb0236458460d1dc7b328bb3765c128d3
Plant Lady <codeplantsbylouie@gmail.com>
2019-07-04 19:00:54 -0400

    fixing edit and returning functions to og

--- calculateNumbers/main.js
+++ calculateNumbers/main.js
@@ -28,19 +28,19 @@ add(1, 5);
 
 
 function subtract(value1, value2) {
-  let difference = value1 + value2;
+  let difference = value1 - value2;
   console.log(difference);
 }
 subtract(10, 7);
 
 function multiply(value1, value2) {
-  let product = value1 + value2;
+  let product = value1 * value2;
   console.log(product);
 }
 multiply(5, 6);
 
 function divide(value1, value2) {
-  let quotient = value1 + value2;
+  let quotient = value1 / value2;
   console.log(quotient);
 }
 divide(9,3);
