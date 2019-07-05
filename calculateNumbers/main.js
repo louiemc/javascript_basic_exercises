72c405823147a6df07cf377073adf076b6412228
Plant Lady <codeplantsbylouie@gmail.com>
2019-07-05 10:43:21 -0400

    input and list for choices are working; functions are not working, promise is being rejected

--- calculateNumbers/main.js
+++ calculateNumbers/main.js
@@ -1,49 +1,57 @@
 const inquirer = require("inquirer");
 
-inquirer.prompt([
-  {
-    type: "input",
-    name: "value1",
-    message: "Pick a number for value1"
-  },
-  {
-    type: "list",
-    name: 'operator',
-    message: "Choose your arithmetic operator"
-    choices: ['Addition', "Subtraction", "Mulitplication", "Division"]
-  },
-  {
-    type: "input",
-    name: "value2",
-    message: "Pick a number for value2"
-  }
-]).then(function(user) {
-  if (user.choices === "Addition") {
-    
-  }
-});
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "value1",
+      message: "Pick a number for value1"
+    },
+    {
+      type: "list",
+      name: "operator",
+      message: "Choose your arithmetic operator",
+      choices: ["Addition", "Subtraction", "Mulitplication", "Division"]
+    },
+    {
+      type: "input",
+      name: "value2",
+      message: "Pick a number for value2"
+    }
+  ])
+  .then(function(user) {
+    console.log("****************************************");
+    if (user.choices === "Addition") {
+      function add(value1, value2) {
+        let sum = value1 + value2;
+        return sum;
+      }
+      add();
+      return sum;
+    }
+    console.log(sum);
+    console.log("****************************************");
+  });
 
-function add(value1, value2) {
-  let sum = value1 + value2;
-  console.log(sum);
-}
-add(1, 5);
+// function add(value1, value2) {
+//   let sum = value1 + value2;
+//   console.log(sum);
+// }
 
+// function subtract(value1, value2) {
+//   let difference = value1 - value2;
+//   console.log(difference);
+// }
+// subtract(10, 7);
 
-function subtract(value1, value2) {
-  let difference = value1 - value2;
-  console.log(difference);
-}
-subtract(10, 7);
+// function multiply(value1, value2) {
+//   let product = value1 * value2;
+//   console.log(product);
+// }
+// multiply(5, 6);
 
-function multiply(value1, value2) {
-  let product = value1 * value2;
-  console.log(product);
-}
-multiply(5, 6);
-
-function divide(value1, value2) {
-  let quotient = value1 / value2;
-  console.log(quotient);
-}
-divide(9,3);
+// function divide(value1, value2) {
+//   let quotient = value1 / value2;
+//   console.log(quotient);
+// }
+// divide(9,3);
