9277b691e75de04ce3c3d2e8ba9135aa58e72f89
Plant Lady <codeplantsbylouie@gmail.com>
2019-07-05 09:59:14 -0400

    created inquirer prompt; using a promise for the user

--- calculateNumbers/main.js
+++ calculateNumbers/main.js
@@ -1,6 +1,27 @@
-// const inquirer = require(inquirer);
+const inquirer = require("inquirer");
 
-
+inquirer.prompt([
+  {
+    type: "input",
+    name: "value1",
+    message: "Pick a number for value1"
+  },
+  {
+    type: "list",
+    name: 'operator',
+    message: "Choose your arithmetic operator"
+    choices: ['Addition', "Subtraction", "Mulitplication", "Division"]
+  },
+  {
+    type: "input",
+    name: "value2",
+    message: "Pick a number for value2"
+  }
+]).then(function(user) {
+  if (user.choices === "Addition") {
+    
+  }
+});
 
 function add(value1, value2) {
   let sum = value1 + value2;
